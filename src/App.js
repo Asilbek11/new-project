d64f1fc8be34d543bbe83b0dfa86afee718a1aa5
Asil <musayevfarhod90@gmail.com>
2024-01-30 18:00:41 +0500

    added react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import './App.css';
+const url = 'https://foodprint.p.rapidapi.com/api/foodprint';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e923516b47msh5f3da33d76e50d2p17dcd3jsne859c019ac9f',
+		'X-RapidAPI-Host': 'foodprint.p.rapidapi.com'
+	}
+};
+const response = await fetch(url, options);
+	const result = await response.text();
 
 function App() {
   return (
