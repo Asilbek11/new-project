63bdd696b5e18ede3ed635cca184f040f9cf803c
Asil <musayevfarhod90@gmail.com>
2024-02-01 19:41:15 +0500

    responsive bilan section qoshildi!

--- js/home.js
+++ js/home.js
@@ -2,11 +2,17 @@ let menuBtn = document.querySelector('.menu-btn');
 let menuIconOpen = document.querySelector('.menu-icon-open');
 let menuIconClose = document.querySelector('.menu-icon-close');
 let navWrapper = document.querySelector('.nav-wrapper');
+let deliverBtn = document.querySelectorAll('.deliver-food');
 let nav = document.querySelector('.nav')
 menuBtn.addEventListener('click',()=>{
     navWrapper.classList.toggle('active')
     menuBtn.classList.toggle('active')
     menuIconOpen.classList.toggle('active');
     menuIconClose.classList.toggle('active');
-    nav.classList.toggle('active')
+    nav.classList.toggle('active'); 
 })
+for (let i = 0; i < deliverBtn.length; i++) {
+    deliverBtn[i].addEventListener('click',()=>{
+        deliverBtn[i].classList.toggle('active')
+    })
+}
