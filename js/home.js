84a00a94b1d392be95397b1dfbbae92b56a6d491
Asil <musayevfarhod90@gmail.com>
2024-02-01 09:04:53 +0500

    navbar tugadi vanihoyat

--- js/home.js
+++ js/home.js
@@ -1,7 +1,17 @@
 let menuBtn = document.querySelector('.menu-btn');
+let menuIconOpen = document.querySelector('.menu-icon-open');
+let menuIconClose = document.querySelector('.menu-icon-close');
 let navWrapper = document.querySelector('.nav-wrapper');
 let nav = document.querySelector('.nav')
 menuBtn.addEventListener('click',()=>{
     navWrapper.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    menuIconOpen.classList.toggle('active');
+    menuIconClose.classList.toggle('active');
+    // if (menuBtn.classList.contains('active')) {
+    //     menuIcon.setAttribute('src','https://cdn.lordicon.com/nqtddedc.json')
+    // }else{
+    //     menuIcon.setAttribute('src','https://cdn.lordicon.com/whrxobsb.json')
+    // }
     nav.classList.toggle('active')
 })
